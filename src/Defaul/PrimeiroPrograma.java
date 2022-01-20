dc4067658b7ce24ab1d00d51022394aef3c49e34
Neiva2018 <samuel.silva.external@k2partnering.com>
2022-01-20 10:46:28 -0300

    teste subir codigo

---
+++ src/Defaul/PrimeiroPrograma.java
@@ -0,0 +1,47 @@
+package Defaul;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+      /* int idade;
+       double pi, PI;
+       char primeiraLetraNome;
+       idade =31;
+       
+       System.out.println(idade);*/
+		
+		int n1,n2;
+		
+		n1 = 10;
+		n2 = 20;
+		
+		/*int soma, sub, mod, mult;
+		
+		soma = n1 +n2;
+		sub = n2 - n1;
+		mod = n2 % n1;
+		mult = n1 * n2;
+		
+		
+		
+		System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(mod);
+        System.out.println(mult);*/
+		
+		
+		System.out.println(n1 != n2);
+		
+		
+		
+//		|| significa ou uma das 
+		
+		
+		
+		
+		
+		
+        
+	}
+
+}
